e22b4ed22d9133c39feb0400927c00f746449ac5
theFlashmoi <moisesym.rojas@gmail.com>
2025-07-04 17:19:18 -0400

    Update encryption.js

--- encryption.js
+++ encryption.js
@@ -4,16 +4,6 @@ require('dotenv').config();
 const ENCRYPTION_KEY = process.env.ENCRYPTION_SECRET;
 
 module.exports = {
-  encryptData: (data) => {
-    try {
-      const dataStr = JSON.stringify(data);
-      return CryptoJS.AES.encrypt(dataStr, ENCRYPTION_KEY).toString();
-    } catch (error) {
-      console.error('Error en encryptData:', error);
-      throw new Error('Error al encriptar');
-    }
-  },
-
   decryptData: (encryptedStr) => {
     try {
       const bytes = CryptoJS.AES.decrypt(encryptedStr, ENCRYPTION_KEY);
